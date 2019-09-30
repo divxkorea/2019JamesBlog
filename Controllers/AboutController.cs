71f7c246b70f664b3f2744495be833ee24d3d972
James <divxkorea@gmail.com>
2019-09-30 22:55:26 +0900

    second test message James Kimenter the commit message for your changes. Lines starting

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JamesBlog.Controllers
+{
+    [Route("About")]
+    public class AboutController : Controller
+    {
+        [HttpGet]
+        [Route("Me")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -39,10 +39,12 @@ namespace JamesBlog
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+            
 
             app.UseAuthorization();
 
